5f5b63f85f0db60a7b697a232207cefe46d0a5b6
Chepelepe <joselepe2003@hotmail.com>
2020-02-09 22:50:48 -0600

    ¨actividad2¨

---
+++ actividad2.js
@@ -0,0 +1,9 @@
+var edad = parseInt(prompt(´Ingrese su edad´);
+
+if (edad < 18){
+	console.log(´es menor de edad´);
+} else if (edad == 18) {
+	console.log(´Ya puede tramitar su DPI´);
+} else {
+	console.log(´es mayor de edad´);
+}
